9c68ea653c16c1df418d7ade3ba4cad78cce74cd
JamesHarryT <140349577+JamesHarryT@users.noreply.github.com>
2026-04-13 18:16:28 +0000

    created class files for problems and tests

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+Name: Harrison Tinley
+Date: 4/13/2026
+This class will be used to test the methods int eh RecursionProblems class
+*/
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println(RecursionProblems.stringClean("test"));
+    }
+}
