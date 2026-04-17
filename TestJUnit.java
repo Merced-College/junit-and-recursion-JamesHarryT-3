86ea6bb201075543dd60f2160fb1976a70b631a0
JamesHarryT <140349577+JamesHarryT@users.noreply.github.com>
2026-04-17 22:15:51 +0000

    finished lecture, did strCount problem and tests

--- TestJUnit.java
+++ TestJUnit.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 
 public class TestJUnit {
     
+    // count8 tests
     @Test
     public void testCount8_1() {
         assertEquals(4, TestingRecursion.count8(8188));
@@ -28,4 +29,29 @@ public class TestJUnit {
         assertEquals(0, TestingRecursion.count8(1645));
     }
 
+    // strCount tests
+    @Test
+    public void testStrCount_1() {
+        assertEquals(2, TestingRecursion.strCount("catcowcat", "cat"));
+    }
+
+    @Test
+    public void testStrCount_2() {
+        assertEquals(0, TestingRecursion.strCount("catcowcat", "dog"));
+    }
+
+    @Test
+    public void testStrCount_3() {
+        assertEquals(3, TestingRecursion.strCount("cowcowcow", "cow"));
+    }
+
+    @Test
+    public void testStrCount_4() {
+        assertEquals(1, TestingRecursion.strCount("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz"));
+    }
+
+    @Test
+    public void testStrCount_5() {
+        assertEquals(6, TestingRecursion.strCount("hahahahahaha", "ha"));
+    }
 }
--- TestingRecursion.java
+++ TestingRecursion.java
@@ -47,10 +47,25 @@ public class TestingRecursion {
     }
     
     public static int strCount(String str, String sub) {
-        return 1;
-    }
+        //base case
+        if(str.length() < sub.length()) {
+            return 0;
+        }
+
+        if (str.substring(0, sub.length()).equals(sub)) {
+            return 1 + strCount(str.substring(sub.length()), sub);
+        }
+        else {
+            return strCount(str.substring(1), sub);
+        }
+    } // end of strCount method
 
     public static String stringClean(String str) {
         return "Test working";
     }
+
+    public static void main(String[] args) {
+        System.out.println(strCount("cowcatcat", "cat"));
+    }
+
 }
